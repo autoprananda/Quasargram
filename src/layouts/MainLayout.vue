254b706af32701c894ed0a7d24ca2b9df1065633
auto prananda <auto.prananda@batmandiri.com>
2021-03-23 23:08:03 +0700

    build quasar project dan create screen home, screen camera

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,58 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-white text-grey-10 " bordered>
+      <q-toolbar>
+        <q-btn flat rounded 
+          icon="eva-camera-outline"
+          dense 
+          size="18px" 
+          to="/camera"
+          class="large-screen-only"/>
+
+        <q-toolbar-title class="text-grand-hotel text-bold">
+          Quasargram
+        </q-toolbar-title>
+
+        <q-btn flat rounded 
+          icon="eva-home-outline"
+          dense 
+          size="18px" 
+          to="/"
+          class="large-screen-only"/>
+      </q-toolbar>
+    </q-header>
+
+    <q-footer class="bg-white small-screen-only" bordered>
+        <q-tabs 
+        class="text-grey-10"
+        active-color="primary"
+        indicator-color="transparent">
+          <q-route-tab to="/" icon="eva-home-outline" />
+          <q-route-tab to="/camera" icon="eva-camera-outline"/>
+        </q-tabs>
+    </q-footer> 
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang="sass">
+  .q-toolbar__title
+    text-align: center
+    font-size: 30px
+  .q-footer 
+  .q-tab__icon
+    font-size: 30px
+</style>
