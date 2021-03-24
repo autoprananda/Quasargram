962bb64a25f03d68fb3589019a6117992b51498e
auto prananda <auto.prananda@batmandiri.com>
2021-03-24 23:19:10 +0700

    fix screen camera and add function camera

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,14 +1,14 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header class="bg-white text-grey-10 " bordered>
-      <q-toolbar>
+      <q-toolbar class="constrain">
         <q-btn flat rounded 
           icon="eva-camera-outline"
           dense 
           size="18px" 
           to="/camera"
-          class="large-screen-only"/>
-
+          class="large-screen-only q-mr-sm"/>
+        <q-separator vertical spaced  class="large-screen-only"/>
         <q-toolbar-title class="text-grand-hotel text-bold">
           Quasargram
         </q-toolbar-title>
@@ -32,7 +32,7 @@
         </q-tabs>
     </q-footer> 
 
-    <q-page-container>
+    <q-page-container class="bg-grey-1">
       <router-view />
     </q-page-container>
   </q-layout>
@@ -49,10 +49,14 @@ export default {
 }
 </script>
 <style lang="sass">
+  .q-toolbar
+    @media (min-width: $breakpoint-sm-min)
+      height: 77px
   .q-toolbar__title
-    text-align: center
     font-size: 30px
+    @media (max-width: $breakpoint-xs-max)
+      text-align: center 
   .q-footer 
-  .q-tab__icon
+    .q-tab__icon
     font-size: 30px
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,99 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Home</h5>
+  <q-page class="constrain q-pa-md">
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card flat bordered v-for="post in posts" :key="post.id" class="card-post q-mb-md">
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label>Auto</q-item-label>
+              <q-item-label caption>
+                {{post.location}}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator />
+          <q-img :src="post.imageUrl" />
+          <q-card-section>
+            <div>{{post.Caption}}</div>
+            <div class="text-caption text-grey">{{post.date | niceDate}}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-4 large-screen-only">
+        <q-item class="fixed">
+            <q-item-section avatar>
+              <q-avatar size="48px">
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label class="text-bold">Auto Prananda</q-item-label>
+              <q-item-label caption>
+                auto_prananda
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+      </div>
+    </div>
+    
   </q-page>
 </template>
 
 <script>
+import { date } from 'quasar'
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      posts: [
+        {
+        id: 1,
+        Caption: 'Golden Gate',
+        date: 1616571845759,
+        location: 'Jakarta',
+        imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+        id: 2,
+        Caption: 'Golden Gate',
+        date: 1616571845759,
+        location: 'Jakarta',
+        imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+        id: 3,
+        Caption: 'Golden Gate',
+        date: 1616571845759,
+        location: 'Jakarta',
+        imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+        id: 4,
+        Caption: 'Golden Gate',
+        date: 1616571845759,
+        location: 'Jakarta',
+        imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        }
+      ]
+    }
+  },
+  filters: {
+    niceDate(value) {
+      return date.formatDate(value, 'D MMMM h:mmA')
+    }
+  }
 }
 </script>
+<style lang="sass">
+  .card-post
+    .q-img
+      min-height: 200px
+</style>
