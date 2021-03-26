2eff5a947d60d2455462cf372fc660fddeac74f6
auto prananda <auto.prananda@batmandiri.com>
2021-03-26 16:53:52 +0700

    create backend, connect to firebase database, display data from database

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+// dependencies
+const express = require('express')
+const admin = require('firebase-admin')
+
+
+
+// config - express
+const app = express()
+
+// config - firebase
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+// endpoint - posts
+const port = 3000
+
+app.get('/posts', (request, response) => {
+  response.set('Access-Control-Allow-Origin', '*')
+  let posts = []
+  db.collection('posts').orderBy('date', 'desc').get().then(snapshot => {
+    snapshot.forEach((doc) => {
+      posts.push(doc.data())
+    });
+  response.send(posts)
+  })
+})
+
+// listen 
+app.listen(process.env.PORT || 3000)
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,29 +2,59 @@
   <q-page class="constrain q-pa-md">
     <div class="row q-col-gutter-lg">
       <div class="col-12 col-sm-8">
-        <q-card flat bordered v-for="post in posts" :key="post.id" class="card-post q-mb-md">
-          <q-item>
-            <q-item-section avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-              </q-avatar>
-            </q-item-section>
+        <template v-if="!loadingPost && posts.length">
+          <q-card flat bordered v-for="post in posts" :key="post.id" class="card-post q-mb-md">
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar>
+                  <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+                </q-avatar>
+              </q-item-section>
 
-            <q-item-section>
-              <q-item-label>Auto</q-item-label>
-              <q-item-label caption>
-                {{post.location}}
-              </q-item-label>
-            </q-item-section>
-          </q-item>
+              <q-item-section>
+                <q-item-label>Auto</q-item-label>
+                <q-item-label caption>
+                  {{post.location}}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
 
-          <q-separator />
-          <q-img :src="post.imageUrl" />
-          <q-card-section>
-            <div>{{post.Caption}}</div>
-            <div class="text-caption text-grey">{{post.date | niceDate}}</div>
-          </q-card-section>
-        </q-card>
+            <q-separator />
+            <q-img :src="post.imageURL" />
+            <q-card-section>
+              <div>{{post.caption}}</div>
+              <div class="text-caption text-grey">{{post.date | niceDate}}</div>
+            </q-card-section>
+          </q-card>
+        </template>
+        <template v-else-if="!loadingPost && !posts.length">
+          <h5 class="text-center text-grey">No Posts yet.</h5>
+        </template>
+        <template v-else>
+          <q-card flat bordered>
+            <q-item>
+              <q-item-section avatar>
+                <q-skeleton type="QAvatar" animation="fade" size="40px"/>
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>
+                  <q-skeleton type="text" animation="fade" />
+                </q-item-label>
+                <q-item-label caption>
+                  <q-skeleton type="text" animation="fade" />
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-skeleton height="200px" square animation="fade" />
+
+            <q-card-section>
+              <q-skeleton type="text" class="text-subtitle2" animation="fade" />
+              <q-skeleton type="text" width="50%" class="text-subtitle2" animation="fade" />
+            </q-card-section>
+          </q-card>
+        </template>
       </div>
       <div class="col-4 large-screen-only">
         <q-item class="fixed">
@@ -49,45 +79,37 @@
 
 <script>
 import { date } from 'quasar'
+import axios from 'axios'
 export default {
   name: 'Home',
   data () {
     return {
-      posts: [
-        {
-        id: 1,
-        Caption: 'Golden Gate',
-        date: 1616571845759,
-        location: 'Jakarta',
-        imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
-        },
-        {
-        id: 2,
-        Caption: 'Golden Gate',
-        date: 1616571845759,
-        location: 'Jakarta',
-        imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
-        },
-        {
-        id: 3,
-        Caption: 'Golden Gate',
-        date: 1616571845759,
-        location: 'Jakarta',
-        imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
-        },
-        {
-        id: 4,
-        Caption: 'Golden Gate',
-        date: 1616571845759,
-        location: 'Jakarta',
-        imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
-        }
-      ]
+      posts: [],
+      loadingPost: false
     }
   },
   filters: {
     niceDate(value) {
       return date.formatDate(value, 'D MMMM h:mmA')
+    }
+  },
+  created() {
+    this.getPost()
+  },
+  methods: {
+    getPost(){
+      this.loadingPost = true
+      axios.get('http://localhost:3000/posts').then(response => {
+        console.log('response', response)
+        this.posts = response.data
+        this.loadingPost = false
+      }). catch (err => {
+        this.$q.dialog({
+          title: 'Error',
+          message: 'Could not download'
+        })
+        this.loadingPost = false
+      })
     }
   }
 }
